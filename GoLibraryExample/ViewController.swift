36ca6fcc17fea0b29610a230abeba9676ddc9962
Marc-Andrea Tarnutzer <mail@marctarnutzer.com>
2019-11-21 19:17:15 +0100

    Added example library usage

--- GoLibraryExample/ViewController.swift
+++ GoLibraryExample/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import HTMLEscaper
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("Loaded viewDidLoad")
+        let someString = "<script>alert(123);</script>"
+        print("HTML escaped string: \(someString.escapedHTMLString() ?? "")")
     }
-
-
 }
 
+extension String {
+    public func escapedHTMLString() -> String? {
+        return self.withCString() {
+            guard let v = escape_html(UnsafeMutablePointer(mutating: $0)) else { return nil }
+            return String(bytesNoCopy: v, length: strlen(v), encoding: .utf8, freeWhenDone: true)
+        }
+    }
+
+    public func unescapedHTMLString() -> String? {
+        return self.withCString() {
+            guard let v = unescape_html(UnsafeMutablePointer(mutating: $0)) else { return nil }
+            return String(bytesNoCopy: v, length: strlen(v), encoding: .utf8, freeWhenDone: true)
+        }
+    }
+}
