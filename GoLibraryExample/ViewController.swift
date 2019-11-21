13fd75ece6780aa89f00cd2e927e0db174ffa135
Marc-Andrea Tarnutzer <mail@marctarnutzer.com>
2019-11-21 15:51:00 +0100

    Initial Commit

---
+++ GoLibraryExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoLibraryExample
+//
+//  Created by Marc Tarnutzer on 21.11.19.
+//  Copyright © 2019 Marc Tarnutzer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
